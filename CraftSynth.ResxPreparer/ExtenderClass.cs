2577ed8c17e17a10280a669b8f0afb57523a1b39
F4CIO <f4cio@f4cio.com>
2017-05-01 17:49:30 +0200

    Initial version copied from SourceCode

---
+++ CraftSynth.ResxPreparer/ExtenderClass.cs
@@ -0,0 +1,542 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CraftSynth.ResxPreparer
+{
+    public static class ExtenderClass
+    {
+        public static bool IsNull<T>(this T? nullableObject) where T : struct
+        {
+            return !nullableObject.HasValue;
+        }
+
+        public static string ToNonNullString<T>(this T? nullableObject) where T : struct
+        {
+            return ToNonNullString<T>(nullableObject, string.Empty);
+        }
+
+        public static string ToNonNullString<T>(this T? nullableObject, string nullCaseString) where T : struct
+        {
+            if (nullableObject.HasValue)
+            {
+                return nullableObject.Value.ToString();
+            }
+            else
+            {
+                return nullCaseString;
+            }
+        }
+
+        public static string ToNonNullString(this DateTime? nullableObject)
+        {
+            return ToNonNullString(nullableObject, string.Empty, null);
+        }
+
+        public static string ToNonNullString(this DateTime? nullableObject, string nullCaseString)
+        {
+            return ToNonNullString(nullableObject, nullCaseString, null);
+        }
+
+        public static string ToNonNullString(this DateTime? nullableObject, string nullCaseString, string format)
+        {
+            if (nullableObject.HasValue)
+            {
+                if (format == null)
+                {
+                    return nullableObject.Value.ToString();
+                }
+                else
+                {
+                    return nullableObject.Value.ToString(format);
+                }
+            }
+            else
+            {
+                return nullCaseString;
+            }
+        }
+
+        public static string ToNonNullString(this string nullableObject)
+        {
+            return ToNonNullString(nullableObject, string.Empty);
+        }
+
+        public static string ToNonNullString(this string nullableObject, string nullCaseString)
+        {
+            if (nullableObject != null)
+            {
+                return nullableObject;
+            }
+            else
+            {
+                return nullCaseString;
+            }
+        }
+
+        public static bool IsNullOrWhiteSpace(this string nullableObject)
+        {
+            return string.IsNullOrWhiteSpace(nullableObject);
+        }
+
+        public static bool IsNOTNullOrWhiteSpace(this string nullableObject)
+        {
+            return !string.IsNullOrWhiteSpace(nullableObject);
+        }
+
+        public static string EnclosedWithPercentSignMakeStartsWithClause(this string nullableObject)
+        {
+            var result = nullableObject;
+
+            if (nullableObject.IsNOTNullOrWhiteSpace())
+            {
+                if (!nullableObject.EndsWith("%"))
+                {
+                    result = result + "%";
+                }
+            }
+
+            return result;
+        }
+
+        public static string EnclosedWithPercentSign(this string nullableObject)
+        {
+            string result = nullableObject;
+
+            if (nullableObject.IsNOTNullOrWhiteSpace())
+            {
+                if (!nullableObject.StartsWith("%"))
+                {
+                    result = "%" + result;
+                }
+
+                if (!nullableObject.EndsWith("%"))
+                {
+                    result = result + "%";
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// If instance is not null or white space returns instance + stringAppend;
+        /// otherwse returns instance.
+        /// </summary>
+        /// <param name="nullableObject"></param>
+        /// <param name="stringToPrepend"></param>
+        /// <returns></returns>
+        public static string AppendIfNotNullOrWhiteSpace(this string nullableObject, string stringToAppend)
+        {
+            string result = nullableObject;
+            if (nullableObject.IsNOTNullOrWhiteSpace())
+            {
+                result = result + stringToAppend;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// If instance is not null or white space returns stringToPrepend + instance;
+        /// otherwse returns instance.
+        /// </summary>
+        /// <param name="nullableObject"></param>
+        /// <param name="stringToPrepend"></param>
+        /// <returns></returns>
+        public static string PrependIfNotNullOrWhiteSpace(this string nullableObject, string stringToPrepend)
+        {
+            string result = nullableObject;
+            if (nullableObject.IsNOTNullOrWhiteSpace())
+            {
+                result = stringToPrepend + result;
+            }
+            return result;
+        }
+
+        public static List<string> ParseCSV(this string nullableString, char[] separator)
+        {
+            return ParseCSV<string>(nullableString, separator, new char[] { ' ' }, false, null, null);
+        }
+
+        /// <summary>
+        /// Splits string instance on every comma sign. 
+        /// Does trimming all the way. 
+        /// Returns list if resuting parts which does not include empty or whitespace items.
+        /// On any error returns null.
+        /// </summary>
+        /// <param name="nullableString"></param>
+        /// <returns></returns>
+        public static List<string> ParseCSV(this string nullableString)
+        {
+            return ParseCSV<string>(nullableString, new char[] { ',' }, new char[] { ' ' }, false, null, null);
+        }
+
+        public static List<T> ParseCSV<T>(this string nullableString, char[] separator, char[] trimChars, bool includeEmptyOrWhiteSpaceItems, List<T> nullCaseResult, List<T> errorCaseResult)
+        {
+            List<T> r = null;
+
+            if (nullableString == null)
+            {
+                r = nullCaseResult;
+            }
+            else
+            {
+                try
+                {
+                    string v = nullableString;
+                    if (trimChars != null)
+                    {
+                        v = v.Trim(trimChars);
+                    }
+
+                    string[] parts = nullableString.Split(separator, StringSplitOptions.None);
+                    foreach (var part in parts)
+                    {
+                        string p = part;
+                        if (trimChars != null)
+                        {
+                            p = p.Trim(trimChars);
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(p) || includeEmptyOrWhiteSpaceItems)
+                        {
+                            if (r == null)
+                            {
+                                r = new List<T>();
+                            }
+
+                            T pAsT = (T)Convert.ChangeType((object)p, typeof(T));
+                            r.Add(pAsT);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    r = errorCaseResult;
+                }
+            }
+
+            return r;
+        }
+
+        /// <summary>
+        /// Converts list of values to comma-separated-values (CSV) string. Empty and whitespace items are not included in resulting string.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static string ToCSV<T>(this List<T> list)
+        {
+            return ToCSV<T>(list, false);
+        }
+
+        /// <summary>
+        /// Converts list of values to comma-separated-values (CSV) string.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="includeEmptyOrWhiteSpaceItems"></param>
+        /// <returns></returns>
+        public static string ToCSV<T>(this List<T> list, bool includeEmptyOrWhiteSpaceItems)
+        {
+            return ToCSV<T>(list, ",", new char[] { ' ' }, includeEmptyOrWhiteSpaceItems, null, null);
+        }
+
+        /// <summary>
+        /// Creates the CSV from a generic list.
+        /// </summary>
+        public static string ToCSV<T>(this List<T> list, string separator, char[] trimChars, bool includeEmptyOrWhiteSpaceItems, string nullCaseResult, string errorCaseResult)
+        {
+            string r = null;
+
+            if (list == null)
+            {
+                r = nullCaseResult;
+            }
+            else
+            {
+                try
+                {
+                    StringBuilder sb = new StringBuilder(string.Empty);
+                    foreach (var v in list)
+                    {
+                        string item = string.Format("{0}", v);
+
+                        if (trimChars != null)
+                        {
+                            item = item.Trim(trimChars);
+                        }
+
+                        if (!item.IsNullOrWhiteSpace() ||
+                        item.IsNullOrWhiteSpace() && includeEmptyOrWhiteSpaceItems)
+                        {
+                            if (sb.Length > 0)
+                            {
+                                sb.Append(separator);
+                            }
+                            sb.Append(item);
+                        }
+                    }
+                    r = sb.ToString();
+                }
+                catch (Exception)
+                {
+                    r = errorCaseResult;
+                }
+            }
+
+            return r;
+        }
+
+        public static string FirstXChars(this string s, int count, string endingIfTrimmed)
+        {
+            string r = string.Empty;
+
+            if (s.ToNonNullString().Length >= count)
+            {
+
+                r = s.ToNonNullString().Substring(0, count - endingIfTrimmed.ToNonNullString().Length).ToNonNullString() + endingIfTrimmed.ToNonNullString();
+            }
+            else
+            {
+                r = s.ToNonNullString();
+            }
+
+            return r;
+        }
+
+        /// <summary>
+        /// Returns ending of string.
+        /// If count is bigger than character count in string whole string is returned. 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="count"></param>
+        /// <param name="startingPartIfTrimmed">Can be used as '...' to indicate that result was trimmed at the beginning.</param>
+        /// <returns></returns>
+        public static string LastXChars(this string s, int count, string startingPartIfTrimmed)
+        {
+            string r = s.ToNonNullString();
+            startingPartIfTrimmed = startingPartIfTrimmed.ToNonNullString();
+
+            if (r.Length >= count)
+            {
+                if (startingPartIfTrimmed.Length > count)
+                {
+                    throw new ArgumentException("Length of startingPartIfTrimmed must be smaller than count.");
+                }
+                r = r.Substring(r.Length - count, count);
+                r = r.Remove(0, startingPartIfTrimmed.Length);
+                r = startingPartIfTrimmed + r;
+            }
+
+            return r;
+        }
+
+        public static string GetSubstringAfter(this string s, string startMarker)
+        {
+            string r = null;
+
+            if (s != null && !string.IsNullOrEmpty(startMarker))
+            {
+                int startMarkerIndex = s.IndexOf(startMarker);
+                int endMarkerIndex = s.Length;
+
+                if (startMarkerIndex >= 0 && endMarkerIndex >= 0)
+                {
+                    startMarkerIndex = startMarkerIndex + startMarker.Length;
+
+                    r = s.Substring(startMarkerIndex, endMarkerIndex - startMarkerIndex);
+                }
+            }
+
+            return r;
+        }
+
+        public static string GetSubstringAfterLastOccurence(this string s, string marker)
+        {
+            string r = null;
+
+            if (s != null && !string.IsNullOrEmpty(marker))
+            {
+                int startMarkerIndex = s.LastIndexOf(marker);
+                int endMarkerIndex = s.Length;
+
+                if (startMarkerIndex >= 0 && endMarkerIndex >= 0)
+                {
+                    startMarkerIndex = startMarkerIndex + marker.Length;
+
+                    r = s.Substring(startMarkerIndex, endMarkerIndex - startMarkerIndex);
+                }
+            }
+
+            return r;
+        }
+
+        public static string GetSubstring(this string s, string startMarker, string endMarker)
+        {
+            string r = null;
+
+            if (s != null && !string.IsNullOrEmpty(startMarker) && !string.IsNullOrEmpty(endMarker))
+            {
+                int startMarkerIndex = s.IndexOf(startMarker);
+                int endMarkerIndex = s.IndexOf(endMarker);
+
+                if (startMarkerIndex >= 0 && endMarkerIndex >= 0)
+                {
+                    startMarkerIndex = startMarkerIndex + startMarker.Length;
+
+                    r = s.Substring(startMarkerIndex, endMarkerIndex - startMarkerIndex);
+                }
+            }
+
+            return r;
+        }
+
+        public static string RemoveSubstring(this string s, string startMarker, string endMarker)
+        {
+            string r = null;
+
+            if (s != null && !string.IsNullOrEmpty(startMarker) && !string.IsNullOrEmpty(endMarker))
+            {
+                int startMarkerIndex = s.IndexOf(startMarker);
+                int endMarkerIndex = s.IndexOf(endMarker);
+
+                if (startMarkerIndex >= 0 && endMarkerIndex >= 0)
+                {
+                    startMarkerIndex = startMarkerIndex + startMarker.Length;
+
+                    r = s.Remove(startMarkerIndex, endMarkerIndex - startMarkerIndex);
+                }
+            }
+
+            return r;
+        }
+
+        /// <summary>
+        /// Gets Description from enum value ie [Description("MasterCard")] returns MasterCard
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Description(this Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                    typeof(DescriptionAttribute), false);
+            return (attributes.Length > 0) ? attributes[0].Description : value.ToString();
+        }
+
+        public static int GetValueFromEnumDescription(string description, Type enumType)
+        {
+            foreach (Enum value in enumType.GetEnumValues())
+            {
+                if (value.Description() == description)
+                {
+                    return Int32.Parse(value.ToString("d"));
+                }
+            }
+            throw new Exception("Description isn't defined! desc: " + description);
+        }
+
+        public static string GetDigitsAfterDecimalPoint(this decimal n, int numberOfDigits = 2)
+        {
+            int result = Decimal.ToInt32((Decimal.Round(n, 2) - Decimal.Floor(n) )* 100);
+            return result > 9 ? result.ToString() : "0" + result;
+        }
+
+        public static string GetDigitsBeforeDecimalPoint(this decimal n)
+        {
+            //need to floor n first?
+            return Decimal.ToInt32(n).ToString();
+        }
+
+        public static string RemoveSpaces(this string s)
+        {
+            if (s == null)
+            {
+                return s;
+            }
+            else
+            {
+                return s.Replace(" ", "");
+            }
+        }
+
+        public static List<string> DistinctStrings(this List<string> items)
+        {
+            var r = new List<string>();
+            foreach (var item in items)
+            {
+                if(!r.Contains(item))
+                {
+                    r.Add(item);
+                }
+            }
+            return r;
+        }
+
+        public static string PrepandHiddenHtmlZeros(this string number, int length)
+        {
+            string r = number;
+            string zeros = string.Empty;
+
+            while (r.Length + zeros.Length < length)
+            {
+                zeros += " ";
+            }
+            //zeros = string.Format("<span style=\"visibility:hidden\">{0}</span>", zeros);
+            return zeros + number;
+        }
+
+        public static bool IsAlphaNumeric(this string strToCheck)
+        {
+            Regex objAlphaNumericPattern = new Regex("[^a-zA-Z0-9]");
+            return !objAlphaNumericPattern.IsMatch(strToCheck);
+        }
+
+        /// <summary>
+        /// String length check is performed. I.e. “test”. SafeLeftSubstring(50) returns “test”
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string SafeLeftSubstring(this String data, int length)
+        {
+            return String.IsNullOrEmpty(data) ? data : data.Substring(0, data.Length > length ? length : data.Length);
+        }
+
+        public static string ToProperStringDisplayValue(this decimal value, int decimalPlaces = 2)
+        {
+            var d = (double) value;
+
+            d = d * Math.Pow(10, decimalPlaces);
+            d = Math.Truncate(d);
+            d = d / Math.Pow(10, decimalPlaces);
+
+            return string.Format("{0:N" + Math.Abs(decimalPlaces) + "}", d);
+        }
+
+        /// <summary>
+        /// Gets property name from expression without magic strings. 
+        /// Examle: ExtenderClass.GetPropertyName(()=>Model.AdvertisementImage)  
+        /// "Extended" expressions such model.Propery.PropertyA are not supported.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static string GetPropertyName<T>(System.Linq.Expressions.Expression<Func<T>> expression)
+        {
+            var body = expression.Body as System.Linq.Expressions.MemberExpression;
+            if (body == null)
+            {
+                throw new ArgumentException("'expression' should be a member expression");
+            }
+
+            return body.Member.Name;
+        }
+    }
+}
---
+++ CraftSynth.ResxPreparer/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace CraftSynth.ResxPreparer
+{
+	/// <summary>
+	/// Copies missing items beetween .resx and localized pairs (like .resx.en-us). 
+	/// To use it put this in pre-build event in project where your .resx files are:
+	/// $(SolutionDir)CraftSynth.ResxPreparer\bin\Debug\CraftSynth.ResxPreparer.exe "$(ProjectDir)App_GlobalResources"
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try{
+				MainInner(args);
+			}
+			catch (Exception exception)
+			{
+				try
+				{
+					string exeFolderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace("file:\\", "");
+					string content = exception.Message + "\r\n\r\n" + exception.StackTrace;
+					File.WriteAllText(exeFolderPath + "\\LastError.txt", content);
+				}
+				catch (Exception)
+				{
+				}
+				throw exception;
+			}
+		}
+
+		private static void MainInner(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				throw new Exception("App must be run with one argument (path to folder with .resx files).");
+			}
+
+			string folderPath = args[0];
+			if (!Directory.Exists(folderPath))
+			{
+				throw new Exception("Folder not found:" + folderPath);
+			}
+
+			folderPath = folderPath.TrimEnd('\\');
+
+			List<string> resxFilesPaths = GetFilePaths(folderPath, "*.resx");
+			if (resxFilesPaths == null || resxFilesPaths.Count == 0)
+			{
+				throw new Exception("No .resx files in folder:" +folderPath);
+			}
+
+			string defaultResxFilePath = resxFilesPaths.FirstOrDefault(r=>!Path.GetFileName(r.ToLower()).Replace(".resx",string.Empty).Contains("."));
+			if (defaultResxFilePath == null)
+			{
+				throw new Exception("Can not find default .resx file - one without culture name in filename. Folder:" + folderPath);
+			}
+
+			//TODO: consider special logic if we have this for example: .it-IT.resx and .it.resx
+
+			//first copy items that exist in localized files but missing in default resx
+			foreach (var resxFilePath in resxFilesPaths)
+			{
+				if (string.Compare(resxFilePath, defaultResxFilePath, StringComparison.InvariantCultureIgnoreCase) != 0)
+				{
+					CopyUnexistingItems(resxFilePath,defaultResxFilePath);
+				}
+			}
+
+			//secondly copy missing items from default resx to localized files
+			foreach (var resxFilePath in resxFilesPaths)
+			{
+				if (string.Compare(resxFilePath, defaultResxFilePath, StringComparison.InvariantCultureIgnoreCase) != 0)
+				{
+					CopyUnexistingItems(defaultResxFilePath, resxFilePath);
+				}
+			}
+		}
+
+		private static void CopyUnexistingItems(string sourceResxfith, string destinationResxFilePath)
+		{
+			XmlDocument sourceXmlDoc = new XmlDocument();
+			sourceXmlDoc.Load(sourceResxfith);
+
+			XmlDocument destinationXmlDoc = new XmlDocument();
+			destinationXmlDoc.Load(destinationResxFilePath);
+
+			bool fileChanged = false;
+
+			XmlNodeList nodes = sourceXmlDoc.SelectNodes("/root/data");
+			foreach (XmlNode node in nodes)
+			{
+				XmlNode pairInDestinationXml = destinationXmlDoc.SelectSingleNode("/root/data[@name='" + node.Attributes["name"].Value + "']");
+				if (pairInDestinationXml == null)
+				{
+					//copy item
+					var importedNode = destinationXmlDoc.ImportNode(node, true);
+					//and add for example it-IT prefix to values in order for translator to spot and translate them next time.
+					importedNode["value"].InnerText = GetCultureName(destinationResxFilePath) + importedNode["value"].InnerText;
+					if (importedNode["comment"] != null && !string.IsNullOrWhiteSpace(importedNode["comment"].InnerText))
+					{
+						importedNode["comment"].InnerText = RemoveTimestampsFromComment(importedNode["comment"].InnerText);
+					}
+					destinationXmlDoc.SelectSingleNode("/root").AppendChild(importedNode);
+					fileChanged = true;
+				}
+			}
+
+			if (fileChanged)
+			{
+				destinationXmlDoc.Save(destinationResxFilePath);
+			}
+		}
+
+		/// <summary>
+		/// From d:\dir\filename.en-EN.resx returns E_.
+		/// For default resx (without culture name) returns empty string.
+		/// </summary>
+		/// <param name="resxFilePath"></param>
+		/// <returns></returns>
+		private static string GetCultureName(string resxFilePath)
+		{
+			string cultureName = string.Empty;
+			string filename = Path.GetFileName(resxFilePath.ToLower()).Replace(".resx", string.Empty);
+			if (filename.Contains("."))
+			{
+				cultureName = filename.Substring(filename.LastIndexOf('.') + 1, 1).ToUpper();
+				cultureName = cultureName + "_";
+			}
+			return cultureName;
+		}
+
+
+
+		#region Helper methods
+		/// <summary>
+		/// Gets list of strings where each is full path to file including filename (for example: <example>c:\dir\filename.ext</example>.
+		/// </summary>
+		/// <param name="folder">Full path of folder that should be searched. For example: <example>c:\dir</example>.</param>
+		/// <param name="searchPatern">Filter that should be used. For example: <example>*.txt</example></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">Thrown when parameter is null or empty.</exception>
+		public static List<string> GetFilePaths(string folderPath, string searchPatern)
+		{
+			if (string.IsNullOrEmpty(folderPath)) throw new ArgumentException("Value must be non-empty string.", "folderPath");
+			if (string.IsNullOrEmpty(searchPatern)) throw new ArgumentException("Value must be non-empty string.", "searchPatern");
+
+			List<string> filePaths = new List<string>();
+			string[] filePathStrings = Directory.GetFiles(folderPath, searchPatern);
+			if (filePathStrings != null)
+			{
+				filePaths.AddRange(filePathStrings);
+			}
+
+			return filePaths;
+		}
+
+		private static string RemoveTimestampsFromComment(string comment)
+		{
+			if (comment.IsNOTNullOrWhiteSpace())
+			{
+				string timestamps = comment.GetSubstring("[", "]");
+				if (timestamps.IsNOTNullOrWhiteSpace())
+				{
+					//example LastSync=GMT2005.12.14 13:15:50,LastChange=GMT2005.10.11 03:33:34
+					string patern = @"^LastSync=GMT(?<year>\d{4}).(?<month>\d{2}).(?<day>\d{2}) (?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2}),LastChange=GMT(?<year2>\d{4}).(?<month2>\d{2}).(?<day2>\d{2}) (?<hour2>\d{2}):(?<minute2>\d{2}):(?<second2>\d{2})";
+					Regex regex = new Regex(patern, RegexOptions.IgnoreCase);
+					Match match = regex.Match(timestamps);
+					if (match.Success)
+					{
+						comment =comment.RemoveSubstring("[", "]").TrimStart('[').TrimStart(']').TrimStart(' ');
+					}
+				}
+			}
+			return comment;
+		}
+		#endregion
+	}
+}
